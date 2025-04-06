5aff4e7794fe1f98b151d540b05141f4f9ff57cf
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-06 16:14:55 +0300

    refactor: simplify function signatures and improve readability in NiCoin contract

--- src/NiCoin.sol
+++ src/NiCoin.sol
@@ -9,37 +9,21 @@ contract NiCoin is ERC20 {
         _mint(msg.sender, initailSupply);
     }
 
-    function approve(
-        address spender,
-        uint256 amount
-    ) public override returns (bool) {
+    function approve(address spender, uint256 amount) public override returns (bool) {
         if (spender == address(0)) {
             revert("ERC20: approve to the zero address");
         }
         return super.approve(spender, amount);
     }
 
-    function increaseAllowance(
-        address spender,
-        uint256 addedValue
-    ) public returns (bool) {
-        _approve(
-            _msgSender(),
-            spender,
-            allowance(_msgSender(), spender) + addedValue
-        );
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(_msgSender(), spender, allowance(_msgSender(), spender) + addedValue);
         return true;
     }
 
-    function decreaseAllowance(
-        address spender,
-        uint256 subtractedValue
-    ) public returns (bool) {
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
         uint256 currentAllowance = allowance(_msgSender(), spender);
-        require(
-            currentAllowance >= subtractedValue,
-            "ERC20: decreased allowance below zero"
-        );
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
         _approve(_msgSender(), spender, currentAllowance - subtractedValue);
         return true;
     }
--- test/NiCoinTest.t.sol
+++ test/NiCoinTest.t.sol
@@ -113,9 +113,5 @@ contract NiCoinTest is Test {
     }
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
