cdaf2135505b26fb8d8a4864c3eb5937e4419f22
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-06 11:49:57 +0300

    feat: add ManualToken contract with basic functions

---
+++ src/ManualToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+
+prangma solidity ^0.8.0;
+
+contract ManualToken {
+   function name() public pure returns (string) {
+     returns "ManualTokem";
+   }
+
+   function totalSupply() public view returns (uint256)
+}
